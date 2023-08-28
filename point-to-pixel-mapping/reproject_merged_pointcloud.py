b7e13c011a084bfd1aa0bed25268a515b54143d9
Laurenz Adrian Heidrich <Laurenz.Heidrich@tum.de>
2023-08-28 10:18:45 +0200

    fixed reporjection algo + first version graph

--- point-to-pixel-mapping/reproject_merged_pointcloud.py
+++ point-to-pixel-mapping/reproject_merged_pointcloud.py
@@ -1,57 +1,63 @@
 import numpy as np
 import cv2
 from scipy.sparse import lil_matrix
-from point_cloud_utils import transform_pcd, get_pcd, point_to_label
+from point_cloud_utils import transform_pcd, get_pcd, point_to_label, change_point_indices
 from point_to_pixels import point_to_pixel
+from hidden_points_removal import hidden_point_removal_o3d
 
-def reproject_merged_pointcloud(pcd_merge, dataset, start_index, sequence_length):
+def reproject_points_to_label(pcd, T_pcd2world, label, T_world2cam, K, hidden_point_removal=True):
     '''
     Args:
-        pcd_merge:          merged point cloud
-        dataset:            dataset object
-        start_index:        index of first point cloud
-        sequence_length:    number of point clouds to merge
+        pcd:            point cloud in camera coordinate [npoints, 3]
+        T_pcd2world:    transformation matrix from point cloud to world coordinate
+        label:          label image
+        T_world2cam:    transformation matrix from world to camera coordinate
+        K:              camera intrinsic matrix
     Returns:
-        feature_matrix:     feature matrix of merged point cloud
+        point_to_label_dict: dict that maps point indices to label
     '''
 
-    left_cam = "cam2"
+    T_pcd2cam = T_world2cam @ T_pcd2world
 
-    pose_merge = dataset.get_pose(start_index)
-    pcd_merge_t0 = transform_pcd(pcd_merge, pose_merge)
-    num_points = pcd_merge_t0.shape[0]
+    pcd_camframe = transform_pcd(pcd, T_pcd2cam)
 
-    feature_matrix = lil_matrix((num_points, 0), dtype=np.int32)
+    if hidden_point_removal:
+        hpr_mask = hidden_point_removal_o3d(pcd_camframe, camera=[0,0,0], radius_factor=1000)
+        pcd_camframe = pcd_camframe[hpr_mask] 
 
-    for i in range(sequence_length):
-        points_index = start_index + i
+    point_to_pixel_dict = point_to_pixel(pcd_camframe, K, label.shape[0], label.shape[1])
 
-        T_lidar2leftcam, K_leftcam = dataset.get_calibration_matrices(left_cam)
-        
-        left_label_PIL = dataset.get_sam_label(left_cam, points_index)
-        left_label = cv2.cvtColor(np.array(left_label_PIL), cv2.COLOR_RGB2BGR)
+    point_to_label_dict = point_to_label(point_to_pixel_dict, label)
 
-        pose_to = np.linalg.inv(dataset.get_pose(points_index))
+    if hidden_point_removal:
+        point_to_label_dict = change_point_indices(point_to_label_dict, hpr_mask)
 
-        pcd_merge_velo_ti = transform_pcd(pcd_merge_t0, pose_to)
-        pcd_merge_leftcamframe = transform_pcd(pcd_merge_velo_ti, T_lidar2leftcam)
+    return point_to_label_dict
 
-        point_to_pixel_dict_leftcam = point_to_pixel(pcd_merge_leftcamframe, K_leftcam, left_label.shape[0], left_label.shape[1])
 
-        point_to_label_dict_leftcam = point_to_label(point_to_pixel_dict_leftcam, left_label)
+def merge_associations(associations, num_points):
+    '''
+    Args:
+        associations:       list of dicts that map point indices to instance ids
+        num_points:         number of points in point cloud
+    Returns:
+        association_matrix: sparse matrix that maps point indices to instance ids
+    '''
 
-        instance_ids = list(set(point_to_label_dict_leftcam.values()))
-       
-        features = lil_matrix((num_points, len(instance_ids)), dtype=np.int32)
+    instance_ids = []
+    num_features = 0
+    for association in associations:
+        instance_ids.append(list(set(association.values())))
+        num_features += len(instance_ids[-1])
 
-        for index, instance_id in point_to_label_dict_leftcam.items():
-            features[index, instance_ids.index(instance_id)] = 1
+    association_matrix = lil_matrix((num_points, num_features), dtype=np.int32)
+    
+    offset = 0
+    num_iteration = 0
+    for association in associations:
+        for index, instance_id in association.items():
+            association_matrix[index, offset + instance_ids[num_iteration].index(instance_id)] = 1
+        offset += len(instance_ids[num_iteration])
+        num_iteration += 1
 
-        old_shape = feature_matrix.get_shape()
-        new_shape = (old_shape[0], old_shape[1] + len(instance_ids))
-        new_feature_matrix = lil_matrix(new_shape, dtype=np.int32)
-        new_feature_matrix[:, :old_shape[1]] = feature_matrix
-        new_feature_matrix[:, old_shape[1]:] = features
-        feature_matrix = new_feature_matrix
-
-    return feature_matrix
+    return association_matrix
