785355d38c532816a567b1b215dfc5267e68a29f
Laurenz Adrian Heidrich <Laurenz.Heidrich@tum.de>
2023-11-23 10:30:59 +0100

    normal direction

--- hidden-point-removal/docker_run.sh
+++ hidden-point-removal/docker_run.sh
@@ -2,4 +2,4 @@ docker run \
     -v /Users/laurenzheidrich/Documents/Studium/Hiwi_TUM.nosync/Programming/unsup_3d_instances/point-to-pixel-mapping/hpr_masks/inputs:/workspace/dataset \
     -v /Users/laurenzheidrich/Documents/Studium/Hiwi_TUM.nosync/Programming/unsup_3d_instances/point-to-pixel-mapping/hpr_masks/outputs:/workspace/output \
     --platform linux/amd64 \
-    makeitdense
+    makeitdense 
