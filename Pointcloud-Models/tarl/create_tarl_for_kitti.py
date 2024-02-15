8b6299a695433cd6cae4e73f85e943c6ea3a96ea
Cedric-Perauer <cedric.perauer@tum.de>
2024-02-15 02:30:02 +0100

    tarl feature creation scripts

---
+++ Pointcloud-Models/tarl/create_tarl_for_kitti.py
@@ -0,0 +1,21 @@
+import os 
+import subprocess 
+import time 
+import select 
+
+seqs = list(range(7,8))
+
+
+input_base_path = '/media/cedric/Datasets1/semantic_kitti/sequences/'
+output_base_path = '/media/cedric/Datasets1/semantic_kitti/tarl_features/'
+
+for seq in seqs : 
+    cur_seq = str(seq).zfill(2)
+    cur_input = input_base_path + cur_seq + '/'  + 'velodyne/' 
+    cur_output = output_base_path + cur_seq + '/'  
+    command = 'python run.py -i' + cur_input + ' -o ' + cur_output 
+    subprocess.call(command,shell=True)
+
+
+
+
---
+++ Pointcloud-Models/tarl/create_tarl_for_nuscenes.py
@@ -0,0 +1,15 @@
+import os 
+import subprocess 
+import time 
+
+
+
+input_base_path = '/media/cedric/Datasets1/nuScenes_train/samples/LIDAR_TOP/'
+output_base_path = '/media/cedric/Datasets1/nuScenes_train/output/TARL/'
+
+command = 'python run.py -i' + input_base_path + ' -o ' + output_base_path + '-d nuscenes'  
+subprocess.call(command,shell=True)
+
+
+
+
