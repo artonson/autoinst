91956b985839fd17b851638563924377d83ab274
Cedric-Perauer <cedric.perauer@tum.de>
2024-02-15 02:25:19 +0100

    nuscenes dino script

--- 2D-VFMs/dinov2/docker_run_nuscenes.sh
+++ 2D-VFMs/dinov2/docker_run_nuscenes.sh
@@ -5,8 +5,8 @@ images=("CAM_FRONT_LEFT" "CAM_FRONT" "CAM_FRONT_RIGHT")
 
 # Loop over images
 for img in "${images[@]}"; do
-        docker run --gpus all \
-            -v "/media/cedric/Datasets1/nuScenes_train/${img}/:/input" \
-            -v "/media/cedric/Datasets1/nuScenes_train/Dinov2/${img}/:/output" \
-            dinov2 --facet 'query' -f '.jpg' -s 14 
+        docker run --gpus all  \
+            -v "/media/cedric/Datasets1/nuScenes_train/samples/${img}/:/input" \
+            -v "/media/cedric/Datasets1/nuScenes_train/outputs/Dinov2/${img}/:/output" \
+            dinov2 -f "jpg" --facet 'query' -s 14
 done
